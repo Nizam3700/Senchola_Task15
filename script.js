2ff96431c47fe9008ec52d57d9cbac250a7a3d83
Nizamuddin <nizamuddin8637@gmail.com>
2023-12-06 22:52:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// write text in website using js   
+document.getElementById("hello").innerHTML ="Hello Guys! Let learn Javascript with Fun";
+
+// js is famous ans 
+function famous(){
+    document.getElementById("popularOfJs").innerHTML ="Ans: its ability to bring online applications to life, generate dynamic user experiences, <br> and function across several platforms.";
+}
+
+// variables btn 
+function vara() {
+    alert("👋 Hello they are three Variables in Js :"+"(var),(let),(const)");
+}
+
+function use(){
+    document.getElementById("use").src='https://cdn.educba.com/academy/wp-content/uploads/2018/10/Uses-of-JavaScript-1.jpg';
+}
+
+// Quiz javasript
+// question 1  
+function correct1(){
+    document.getElementById("correct1").style.color ="darkgreen";
+    document.getElementById("correct1").style.fontSize ="20px";
+    document.getElementById("correct1").style.fontWeight ="800";
+}
+// question 2  
+function correct2(){
+    document.getElementById("correct2").style.color ="darkgreen";
+    document.getElementById("correct2").style.fontSize ="20px";
+    document.getElementById("correct2").style.fontWeight ="800";
+}
+// question 3  
+function correct3(){
+    document.getElementById("correct3").style.color ="darkgreen";
+    document.getElementById("correct3").style.fontSize ="20px";
+    document.getElementById("correct3").style.fontWeight ="800";
+}
+// question 4 
+function correct4(){
+    document.getElementById("correct4").style.color ="darkgreen";
+    document.getElementById("correct4").style.fontSize ="20px";
+    document.getElementById("correct4").style.fontWeight ="800";
+}1
+// question 5 
+function correct5(){
+    document.getElementById("correct5").style.color ="darkgreen";
+    document.getElementById("correct5").style.fontSize ="20px";
+    document.getElementById("correct5").style.fontWeight ="800";
+}
